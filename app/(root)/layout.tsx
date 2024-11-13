83fd75a86602918a709e60cdd9f24528801919f2
Lennard <lennard1@mersch.us>
2024-11-13 19:36:19 +0100

    Implement the complete layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,8 @@
+import MobileNav from "@/components/ui/MobileNav";
+import Sidebar from "@/components/ui/Sidebar";
 import type { Metadata } from "next";
 import { Inter, IBM_Plex_Serif} from "next/font/google";
-
+import Image from "next/image";
 
 
 export default function RootLayout({
@@ -8,10 +10,24 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: 'Lennard', lastName: 'Mersch'};
+
   return (
-    <main>
-        SIDEBAR
-        {children}
+    <main className="flex h-screen w-full font-inter">
+        <Sidebar user={loggedIn}/>
+
+        <div className="flex size-full flex-col">
+          <div className="root-layout">
+            <Image src="/icons/logo.svg"
+            height={30}
+            width={30}
+            alt="logo"/>
+          <div>
+              <MobileNav user={loggedIn}/>
+          </div>
+          </div>
+          {children}
+        </div>
     </main>
   );
 }
---
+++ app/(root)/transaction-history/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TransactionHistory = () => {
+  return (
+    <div>transactions-history</div>
+  )
+}
+
+export default TransactionHistory
